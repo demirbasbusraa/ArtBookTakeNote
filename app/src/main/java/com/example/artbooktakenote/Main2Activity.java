8dfa9f3f0f77bb4b05810218cb8f24f9891d183e
demirbasbusraa <busrademirbas1612@gmail.com>
2020-03-21 20:07:40 +0300

    Adding Menu, Give User Permissions, Choose an Image from Gallery, Get and Transmission of Data in Android Studio

---
+++ app/src/main/java/com/example/artbooktakenote/Main2Activity.java
@@ -0,0 +1,211 @@
+package com.example.artbooktakenote;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Bitmap selectedImage;
+    ImageView imageView;
+    EditText artNameText, painterNameText, yearText;
+    Button button;
+    SQLiteDatabase database;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        imageView = findViewById(R.id.imageView);
+        artNameText = findViewById(R.id.artNameText);
+        painterNameText = findViewById(R.id.painterNameText);
+        yearText = findViewById(R.id.yearText);
+        button = findViewById(R.id.button);
+
+        database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+
+        Intent intent = getIntent();
+        String info = intent.getStringExtra("info");
+
+        if(info.matches("new")){
+            //yeni bişey eklemeye çalışıyo
+            artNameText.setText("");
+            painterNameText.setText("");
+            yearText.setText("");
+            button.setVisibility(View.VISIBLE);
+
+            Bitmap selectImage = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.selectimage);
+            imageView.setImageBitmap(selectImage);
+
+
+        }else{
+            //eskiyi göstereceğim
+            int artId = intent.getIntExtra("artId", 1);
+            button.setVisibility(View.INVISIBLE);
+
+            try {
+
+                Cursor cursor = database.rawQuery("SELECT * FROM arts WHERE id=?", new String[]{String.valueOf(artId)});
+                int artnameIx = cursor.getColumnIndex("artname");
+                int painternameIx = cursor.getColumnIndex("paintername");
+                int yearIx=cursor.getColumnIndex("year");
+                int imageIx = cursor.getColumnIndex("image");
+
+                while (cursor.moveToNext()){
+
+                    artNameText.setText(cursor.getString(artnameIx));
+                    painterNameText.setText(cursor.getString(painternameIx));
+                    yearText.setText(cursor.getString(yearIx));
+
+                    byte[] bytes = cursor.getBlob(imageIx);
+
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    imageView.setImageBitmap(bitmap);
+
+                }
+                cursor.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void selectImage(View view){
+//ContextCOmpat nedir? API23ün öncesindeki ve sonrasındaki değişiklikelri
+//androidin bize hissettirmeden çalıştıran bir yapı. Eskiden direk manifeste yazılıyodu
+//bu durum yanlış herşey öyle olmaz. oyüzden 23 öncesiyse bulaşma değilse izin istemek için bunu kullanıyorum
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            // IZIN İSTEDİK
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }else{
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intentToGallery, 2);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentToGallery, 2);
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode ==2 && resultCode ==RESULT_OK && data != null) {
+
+            Uri imageData = data.getData();
+
+            try {
+                if(Build.VERSION.SDK_INT >= 28){
+                    ImageDecoder.Source source= ImageDecoder.createSource(this.getContentResolver(), imageData);
+                    selectedImage = ImageDecoder.decodeBitmap(source);
+                    imageView.setImageBitmap(selectedImage);
+
+                }else {
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageData);
+                    imageView.setImageBitmap(selectedImage);
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void save(View view){
+
+        String artName = artNameText.getText().toString();
+        String painterName = painterNameText.getText().toString();
+        String year = yearText.getText().toString();
+
+        Bitmap smallImage = makeSmallerImage(selectedImage, 300);
+
+
+        // BURADA GÖRSELİ VERİYE ÇEVİRİYORUZ.
+        //görselin jpegmi pngmi old verdik,
+        // kalitesini verdik(0-100 arasında) eğer SQLITEda 1MB üstünde veriyle çalışırsak SQLITE çöker o yüzden görselleri küçültmeliyiz,
+        // birde byte dizisi gerekliçevirmek için.
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        smallImage.compress(Bitmap.CompressFormat.PNG, 50,byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+
+        // VERİTABANI OLUŞTUR VE KAYDETME İŞLEMİNİ YAP
+        try{
+            database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts (id INTEGER PRIMARY KEY, artname VARCHAR, paintername VARCHAR, year VARCHAR, image BLOB)");
+
+            String sqlString = "INSERT INTO arts(artname, paintername, year, image) VALUES(?, ?, ?, ?)";
+            SQLiteStatement sqLiteStatement = database.compileStatement(sqlString);
+            sqLiteStatement.bindString(1, artName);
+            sqLiteStatement.bindString(2, painterName);
+            sqLiteStatement.bindString(3, year);
+            sqLiteStatement.bindBlob(4, byteArray);
+            sqLiteStatement.execute();
+
+
+        }catch (Exception e){
+
+        }
+        //finish();
+        Intent intent=new Intent(Main2Activity.this, MainActivity.class);
+        //intent açarken burada daha önce açılmış şuanda açık olan tüm intentleri aktivityleri kapat
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+
+
+    public Bitmap makeSmallerImage(Bitmap image, int maksimumSize){
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        double bitmapRatio = (double) width / (double) height;
+        if(bitmapRatio > 1){ //resim yataysa
+            width = maksimumSize;
+            height = (int)(width / bitmapRatio);
+        }else { //resim dikeyse
+            height = maksimumSize;
+            width =(int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image,width,height,true);
+      //yeni bir bitmap olulturduk
+    }
+
+}
